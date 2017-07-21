540acc246193473da776116212eedeea168a59bc
Taehoon Kim <gggkth@naver.com>
2017-07-21 05:06:10 +0000

    initial

---
+++ app/controllers/visitor_controller.rb
@@ -0,0 +1,19 @@
+class VisitorController < ApplicationController
+  require 'open-uri'
+  def main
+    @home_url = "http://tv.naver.com"
+    @url = "#{@home_url}/search/clip?query=%EC%BF%A1%EB%B0%A9&isTag=false"
+    doc = Nokogiri::HTML(open(@url), nil, 'utf-8')
+    @title = doc.css('dt > a')
+    @thumnail = doc.css('a.cds_thm > img')
+    #@new_link = link.map{|link| link.text}
+  end
+  def search
+    @home_url = "http://tv.naver.com"
+    temp = "#{@home_url}/search/clip?query=#{params[:search]}&isTag=false"
+    @url = URI.encode(temp)
+    doc = Nokogiri::HTML(open(@url), nil, 'utf-8')
+    @title = doc.css('dt > a')
+    @thumnail = doc.css('a.cds_thm > img')
+  end
+end
